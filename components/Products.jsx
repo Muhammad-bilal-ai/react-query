ed4d695ab8a191956f649da8f19e54be73646d75
Muhammad Bilal <withbilal777@gmail.com>
2024-06-20 18:54:38 +0500

    changes in UI

--- components/Products.jsx
+++ components/Products.jsx
@@ -10,7 +10,7 @@ const Products = () => {
   const products = data || [];
 
   return (
-    <div>
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4">
       {products.products.map((product) => (
         <ProductCard key={product.id} product={product.title} />
       ))}
--- pages/index.js
+++ pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 export default function Home() {
   return (
     <div>
-      <h1>Product List</h1>
+      <h1 className="text-3xl font-bold text-center my-4">Product List</h1>
       <Products />
     </div>
   );
